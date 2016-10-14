002d8c984f5b013c3cf857513a124ac6d04e9ced
HaluDeCassia <haluanedecassia@gmail.com>
2016-10-14 01:51:50 -0300

    [DOING] Montando api

--- app.py
+++ app.py
@@ -6,15 +6,26 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route('/', method=['GET'])
+@app.route('/')
 def __init__(self):
     self.ticket = []
 
 
 @app.route('/insere')
 def entrar_na_fila(self, elemento):
-    self.ticket.append(elemento)
+    if(prioridade == 1):
+        self.ticket.insert(0, elemento)
+    elif(prioridade == 0):
+        self.ticket.append(elemento)
+    else:
+        return "Prioridade inválida"
 
 
+@app.route('/fila')
+def fila_json(self):
+    return fila.json()
+
+
+@app.route('/')
 def chamar_proximo(self):
     return self.ticket.pop(0)
