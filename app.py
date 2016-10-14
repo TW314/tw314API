c81c03070dd5d5e87446b0c5e38fedb6bf635bc0
HaluDeCassia <haluanedecassia@gmail.com>
2016-10-13 23:29:13 -0300

    Iniciando API com Flask

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ from flask import Flask
 from pip._vendor import requests
 app = Flask(__name__)
 
-fila =[]
-@app.route('/')
-def fila(add):
-    return fila.append(add)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+@app.route('/', method=['GET'])
+def __init__(self):
+    self.ticket = []
+
+
+@app.route('/insere')
+def entrar_na_fila(self, elemento):
+    self.ticket.append(elemento)
+
+
+def chamar_proximo(self):
+    return self.ticket.pop(0)
