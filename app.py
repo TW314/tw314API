05bb4c677fcb67d248f20fb0923c205cccae672b
HaluDeCassia <haluanedecassia@gmail.com>
2016-10-15 00:31:49 -0300

    Teste do Git

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ app = Flask(__name__)
 if __name__ == '__main__':
     app.run(debug=True)
 
+r = requests.get(
+    'http://localhost:3000/consultaInformacoesUsuariosPorPerfil/2' # mudar rota
+).json()
+
 
 @app.route('/')
 def __init__(self):
     self.ticket = []
 
 
-
 @app.route('/insere')
-def entrar_na_fila(self, elemento):
+def entrar_na_fila(self):
     if(prioridade == 1):
         self.ticket.insert(0, elemento)
     elif(prioridade == 0):
@@ -27,6 +30,6 @@ def fila_json(self):
     return fila.json()
 
 
-@app.route('/')
+@app.route('/proximo')
 def chamar_proximo(self):
     return self.ticket.pop(0)
