5539913d1e02f0c6d5991dd28b043611c0a6313a
HaluDeCassia <haluanedecassia@gmail.com>
2016-10-29 01:21:06 -0200

    [DOING] Arrumando erro lógico

--- app.py
+++ app.py
@@ -9,14 +9,17 @@ if __name__ == '__main__':
 	app.run(debug = True)
 
 # r = requests.get('http://localhost:3000/consultaInformacoesUsuariosPorPerfil/2').json()
+# ## GLOBAIS
 normal = []
 prioritaria = []
+total = [0]
+empty = []
 c = 10
 
 
 @app.route('/')
 def __init__():
-	return str(total)
+	return priority()
 
 
 @app.route('/insere')
@@ -24,22 +27,18 @@ def entrar_na_fila():
 	# simulando valores
 	prioridade = randint(0, 1)
 	elemento = randint(0, 999)
-	if len(total) > c:
-		return eh_maior_que_c(prioridade, elemento)
-	else:
-		return nao_eh_maior_que_c(prioridade, elemento, 1)
+	eh_maior_que_c(prioridade, elemento)
+	return "NORMAL: " + str(normal) + "<br>" + "PRIORITÁRIO: " + str(prioritaria)
 
 
-# todo: bug aqui
 def eh_maior_que_c(prioridade, elemento):
-	for i in range(c):
-		if prioridade == 1:
-			prioritaria.append(elemento)
-		elif prioridade == 0:
-			normal.append(elemento)
-		return priority()
+	if prioridade == 1:
+		prioritaria.append(elemento)
+	elif prioridade == 0:
+		normal.append(elemento)
 
-
+# todo: Pensar em lógica melhor pra quando a fila ainda for pequena
+"""
 def nao_eh_maior_que_c(prioridade, elemento, i):
 	i += 1
 	if prioridade == 1:
@@ -47,6 +46,7 @@ def nao_eh_maior_que_c(prioridade, elemento, i):
 	elif prioridade == 0:
 		total.append(elemento)
 	return str(total)
+"""
 
 
 @app.route('/proximo')
@@ -54,20 +54,19 @@ def chamar_proximo():
 	return str(total.pop(0))
 
 
-total = normal.copy()
-
-
-def priority(n = 0, k = 0):
-	for i in range(len(normal + prioritaria)):
-		total.insert(n, 0)
-		n += 3
-	for j in range(len(total)):
-		if k < len(prioritaria):
-			total.insert(total.index(0), prioritaria[k])
-			k += 1
-		if 0 in total:
-			total.remove(0)
+def priority(n = total.index(total[-1]), k = 0):
+	if total != empty:
+		if normal or prioritaria != empty:
+			total.extend(normal.copy())
+			for i in range(len(normal + prioritaria)):
+				total.insert(n, 0)
+				n += 3
+			for j in range(len(total)):
+				if k < len(prioritaria):
+					total.insert(total.index(0), prioritaria[k])
+					k += 1
+				if 0 in total:
+					total.remove(0)
+		# normal[:] = empty[:]
+		# prioritaria[:] = empty[:]
 	return str(total)
-
-
-print(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits)))
